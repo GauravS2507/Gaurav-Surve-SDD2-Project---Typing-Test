875703c508b2168193e5eae144b1be88c2118f74
GauravS2507 <135093120+GauravS2507@users.noreply.github.com>
2023-12-29 15:25:58 +1100

    Importing Modules
    These are all the different Python Modules required to build the Touch Typing Helper Program.

--- Python Project - Typing Test.py
+++ Python Project - Typing Test.py
@@ -1,7 +1,14 @@
 #Basic Import Functions
 import tkinter as tk
-import customtkinter as ctk
-
-
+from tkinter import *
+from tkinter import messagebox
+import random
+import keyboard
+import time
 
 #Pop Up-Window
+root = Tk()
+root.geometry("500x400")
+root.title("Touch Typing Helper")
+
+root.mainloop()
