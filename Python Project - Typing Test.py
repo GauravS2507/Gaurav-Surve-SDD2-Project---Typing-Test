f54dcd642242c049d6fb3c5b6f4ba2715dbf3d82
GauravS2507 <135093120+GauravS2507@users.noreply.github.com>
2024-01-06 01:13:23 +1100

    Working on the Toplevel Settings Window
    Im creating and testing whether i should have another window that pops up all for settings,or should i create a menu option. Done though dates 28 December to 6 Jan

--- Python Project - Typing Test.py
+++ Python Project - Typing Test.py
@@ -10,43 +10,58 @@ import time
 #Place Holder Variables
 x = 0
 
-#Pop Up-Window - Begin Touch Type Helper
-root = customtkinter.CTk()
-root.geometry("450x600")
-root.title("Touch Typing Helper")
-root.frame1 = customtkinter.CTkFrame(root, width=400, height=500)
-root.frame1.grid(row=0, column=0, padx=30, pady=30)
-
-
-
 #Defining Commands
 def New_window():    #From Open Window --> Main Window
-    global x
-    #Destroy the first window when Begin is pressed
+    global x, frame1
+    #Clearing Frame
     if x==0:
         x=1
-        root.destroy()
+        for widgets in frame1.winfo_children(): #Emptying out frame 
+            widgets.destroy()
+            root.geometry("1000x600")
+            frame1 = customtkinter.CTkFrame(root, width=900, height=500)
+            frame1.grid(row=0, column=0, padx=20, pady=30)
+
+def settings():     #Command for settings button
+    settings_window = customtkinter.CTkToplevel(root)
+    settings_window.title("Settings - Touch Typing Helper - Gaurav Surve")
+    settings_window.geometry("500x300")
+    settings_window.resizable(False, False)
+    frame2 = customtkinter.CTkFrame(settings_window, width = 450, height = 250) #Settings Pop-up window frame
+    frame2.grid(row=0, column=0, padx=25, pady=25)
+
+    
+
+        
 
 
-
-#Labels Used
-Welcome_TTH = customtkinter.CTkLabel(root, text="Welcome to the Touch Type Helper")
-
-#Buttons
-Begin_TTH = customtkinter.CTkButton(root, text = "Begin", command=New_window,  )
-Settings = customtkinter.CTkButton(root, text = "Settings⚙️")
-Credits = customtkinter.CTkButton(root, text = "Credits")
-
-#Widget Positioning
-Welcome_TTH.grid(row = 0, column = 0, padx=0, pady=0)
-Begin_TTH.grid(row=2, column=0, padx=0, pady=0)
-Settings.grid()
-Credits.grid()
+#Pop Up-Window - Begin Touch Type Helper
+root = customtkinter.CTk()
+root.geometry("450x350")
+root.title("Touch Typing Helper - Gaurav Surve")
+root.resizable(False, False)
 
 
+# Create a frame for main window
+frame1 = customtkinter.CTkFrame(root, width=400, height=500, border_width = 10, border_color = "#13141F")
+frame1.grid(row=0, column=0, padx=20, pady=30, )
 
+# Labels Used
+Welcome_TTH = customtkinter.CTkLabel(frame1, text="Welcome to the Touch Type Helper", font=("Work Sans", 24), fg_color="#000435", corner_radius=100)
+Welcome_TTH.grid(row=0, column=0, padx=0, pady=60)
 
+# Buttons
+Begin_TTH = customtkinter.CTkButton(frame1, text="Begin", font=("Arial", 16), fg_color="#282E78", command = New_window)
+Begin_TTH.grid(row=1, column=0, padx=0, pady=10)
 
+Settings = customtkinter.CTkButton(frame1, text="Settings⚙️", font=("Arial", 16), fg_color="#282E78", command = settings)
+Settings.grid(row=2, column=0, padx=0, pady=10)
+
+Credits = customtkinter.CTkButton(frame1, text="Credits", font=("Arial", 16), fg_color="#282E78")
+Credits.grid(row=3, column=0, padx=0, pady=10)
+
+#Themes
+customtkinter.set_default_color_theme("green")  # Themes: "blue" (standard), "green", "dark-blue"
 
 
 root.mainloop()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,14 +1,1 @@
-#Labels Used
-Welcome_TTH = Label(root, text="Welcome to the Touch Type Helper", borderwidth=7, relief="ridge")
 
-#Buttons
-Begin_TTH = Button(root, text = "Begin", command=New_window, borderwidth=7 )
-Settings = Button(root, text = "Settings⚙️")
-
-#Positioning Widgets
-Welcome_TTH.grid(row=0, column=0, columnspan=4, padx=10, pady=10, sticky="nsew")
-Welcome_TTH.config(bg="lightblue")
-Begin_TTH.grid(row=1, column=0, columnspan=4, padx=10, pady=10, sticky="nsew")
-Begin_TTH.config(bg="lightblue")
-Settings.grid(row=2, column=0, padx=10, pady=10, sticky="nsew")
-Settings.grid(row=2, column=1, padx=10, pady=10, sticky="nsew")
