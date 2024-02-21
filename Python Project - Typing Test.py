84245c82f3c6a02c3e1db4cb558ab578321fb291
GauravS2507 <135093120+GauravS2507@users.noreply.github.com>
2024-02-22 09:45:57 +1100

    Word List for Type
    An array for all the words list that will be displayed on the program.

--- Python Project - Typing Test.py
+++ Python Project - Typing Test.py
@@ -8,10 +8,12 @@ import random
 import keyboard
 import time
 from pathlib import Path
+import json
+import tkinter.messagebox
 
 
 
-#Defining Commands
+#Defining Commands - Making Main Frame
 def create_typing():
     global x, main_window
     #Clearing Frame
@@ -28,13 +30,21 @@ def create_typing():
     text_container.place(relx=0.5, rely=0.05, relwidth=0.9, relheight=0.45, anchor="n")
     settings_container = ctk.CTkFrame(typing_container, fg_color= "blue")
     settings_container.place(relx = 0.05, rely = 0.95, anchor = "sw", relheight = 0.4 )
-    wpm_label = ctk.CTkLabel(typing_container, text = "WPM:")
+    wpm_label = ctk.CTkLabel(typing_container, text = "WPM: ")
     wpm_label.place(in_ = typing_container, relx = 0.1)
+    accuracy_label = ctk.CTkLabel(typing_container, text = "ACCURACY: ")
+    accuracy_label.place(in_ = typing_container, relx = 0.2)
             
-            
+def credits():
+    tk.messagebox.showinfo("Credits", "Made by Gaurav 12SDD2")
+    
 
 def settings():     #Command for settings button
     settings_window = ctk.CTkToplevel(root)
+    settings_window.lift()
+    settings_window.focus_force()
+    settings_window.grab_set()
+    settings_window.grab_release()
     settings_window.title("Settings - Touch Typing Helper - Gaurav Surve")
     settings_window.geometry("500x300")
     #Frame 2 Setting
@@ -60,7 +70,7 @@ Begin_TTH.place(relx = 0.5, rely = 0.4, anchor = "c")
 Settings = ctk.CTkButton(main_window, text="Settings⚙️", font=("Arial", 16), fg_color="#282E78", command = settings)
 Settings.place(relx = 0.5, rely = 0.5, anchor = "c")
 
-Credits = ctk.CTkButton(main_window, text="Credits", font=("Arial", 16), fg_color="#282E78")
+Credits = ctk.CTkButton(main_window, text="Credits", font=("Arial", 16), fg_color="#282E78", command = credits)
 Credits.place(relx = 0.5, rely = 0.6, anchor = "c")
 
 #Themes
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,1 @@
-
+takefocus
